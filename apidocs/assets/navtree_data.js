f4fcface6e26bd5b966ba40ae93b5feb9298f9aa
Simo Tripodi <stripodi@adobe.com>
2013-02-13 21:08:39 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.caches.oscache", "org/mybatis/caches/oscache/package-summary.html", [ [ "Description", "org/mybatis/caches/oscache/package-descr.html", null, "" ], [ "Classes", null, [ [ "LoggingOSCache", "org/mybatis/caches/oscache/LoggingOSCache.html", null, "" ], [ "OSCache", "org/mybatis/caches/oscache/OSCache.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,6 @@
+var DATA = [
+      { id:0, label:"org.mybatis.caches.oscache", link:"org/mybatis/caches/oscache/package-summary.html", type:"package" },
+      { id:1, label:"org.mybatis.caches.oscache.LoggingOSCache", link:"org/mybatis/caches/oscache/LoggingOSCache.html", type:"class" },
+      { id:2, label:"org.mybatis.caches.oscache.OSCache", link:"org/mybatis/caches/oscache/OSCache.html", type:"class" }
+
+    ];
