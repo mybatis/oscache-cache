b165197d790b9c9ddbc22f862c5db28d094ff21b
Jeremy Landis <jeremylandis@hotmail.com>
2015-10-11 00:47:06 -0400

    wagon-git commit

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,5 +1,5 @@
 var NAVTREE_DATA =
-[ [ "org.mybatis.caches.oscache", "org/mybatis/caches/oscache/package-summary.html", [ [ "Description", "org/mybatis/caches/oscache/package-descr.html", null, "" ], [ "Classes", null, [ [ "LoggingOSCache", "org/mybatis/caches/oscache/LoggingOSCache.html", null, "" ], [ "OSCache", "org/mybatis/caches/oscache/OSCache.html", null, "" ] ]
+[ [ "org.mybatis.caches.oscache", "org/mybatis/caches/oscache/package-summary.html", [ [ "Classes", null, [ [ "LoggingOSCache", "org/mybatis/caches/oscache/LoggingOSCache.html", null, "" ], [ "OSCache", "org/mybatis/caches/oscache/OSCache.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
